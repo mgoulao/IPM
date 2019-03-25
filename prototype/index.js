98393988938dcd0d745370b346fc315b5f473420
Manuel Goulão <msilvagoulao@gmail.com>
2019-03-25 19:31:02 +0000

    start working on contacts screen

--- prototype/index.js
+++ prototype/index.js
@@ -28,3 +28,49 @@ const dayToString = (day) => {
     }
     return ret;
 }
+
+class Contacts {
+
+    constructor() {
+        this.elem = document.querySelector(".contacts");
+        this.setupListeners();
+        this.list = [
+            {
+                name: "Miguel",
+                id: "123",
+                call: true,
+                locate: true,
+            }
+        ];
+        this.createListElem();
+    }
+
+    createListElem() {
+        let listElems = "";
+        this.list.forEach((e) => {
+            listElems +=
+                `<li id="${e.id}">
+                <span>${e.name}</span>
+                <div class="list-btn-container">
+                    <div class="list-btn">
+                        <i class="material-icons phone-btn">local_phone</i>
+                    </div>
+                </div>
+            </li>`;
+
+        });
+        let contactsList = document.querySelector(".contacts-list");
+        contactsList.innerHTML = listElems;
+    }
+    setupListeners() {
+        $(document).on('click', '.phone-btn', (e) => {
+            console.log(e.originalEvent.path[3]);
+        });
+    }
+
+}
+
+
+window.onload = () => {
+    let contacts = new Contacts();
+}
